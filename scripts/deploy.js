511936712411338170f1335a20ebd5fb94e1f5ff
Lukman-01 <abdulyekeenlukman@gmail.com>
2024-02-20 22:01:00 +0100

    deploy script

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,33 +1,36 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
+// Import the Hardhat runtime environment and ethers library for interacting with Ethereum
 const hre = require("hardhat");
+const { ethers } = require("hardhat");
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
+  // Retrieve the list of accounts from the connected Ethereum node and use the first account as the deployer
+  const [deployer] = await ethers.getSigners();
 
-  const lockedAmount = hre.ethers.parseEther("0.001");
+  // Log the address of the account deploying the contracts for verification purposes
+  console.log('Deploying contracts with the account:', deployer.address);
 
-  const lock = await hre.ethers.deployContract("Lock", [unlockTime], {
-    value: lockedAmount,
-  });
+  // Define the name and symbol for the NFT collection to be deployed
+  const name = 'NFTCollection';
+  const symbol = 'NFT';
+  // Prepare the constructor arguments for the NFT collection contract
+  const contractArgs = [name, symbol];
 
-  await lock.waitForDeployment();
+  // Get a contract factory for the NFTCollection, an abstraction used to deploy new smart contracts
+  const NFTCollection = await ethers.getContractFactory('NFTCollection');
+  // Deploy a new NFTCollection contract using the deployer account and the specified constructor arguments
+  const contract = await NFTCollection.deploy(...contractArgs);
 
-  console.log(
-    `Lock with ${ethers.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.target}`
-  );
+  // Await the contract to be fully deployed and transaction to be mined
+  await contract.deployed();
+
+  // Log the address of the deployed NFTCollection contract for reference
+  console.log('NFTCollection Contract deployed to:', contract.address);
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
+// Execute the main function, handling success with a process exit code 0, and catch any errors, logging them and exiting with code 1
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
 });
